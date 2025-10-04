3bb6c704a4b0975f42050b488f89a4decd3a994f
varanganty shivaram <154621416+shivaram171@users.noreply.github.com>
2025-10-04 21:08:20 +0530

    Time: 3 ms (39.51%) | Memory: 46 MB (21.37%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) return false; // can't reach this point
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+        
+        return true;
+    }
+}
