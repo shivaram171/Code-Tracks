1107846d7f8d066af8e85621e4fa2f100a09f79f
varanganty shivaram <154621416+shivaram171@users.noreply.github.com>
2025-12-17 12:08:14 +0530

    Time: 4 ms (47.69%) | Memory: 44.6 MB (88.95%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.java
@@ -6,7 +6,6 @@ class Solution {
         int n = prices.length;
 
         for (int i = 0; i < n; i++) {
-            // Apply discount when current price <= price at stack top
             while (!stack.isEmpty() && prices[i] <= prices[stack.peek()]) {
                 int idx = stack.pop();
                 prices[idx] = prices[idx] - prices[i];
